e4f7ce2176af1e86efabd3109011858c63217775
Nico Kadel-Garcia <ikadel-garcia@statestreet.com>
2023-11-22 10:02:11 -0500

    20752: Enable error reporting for missing sudo on installpsh-rhel.sh

--- tools/installpsh-rhel.sh
+++ tools/installpsh-rhel.sh
@@ -108,8 +108,8 @@ if (( EUID != 0 )); then
     if [[ ("'$*'" =~ skip-sudo-check) && ("$(whereis sudo)" == *'/'* && "$(sudo -nv 2>&1)" != 'Sorry, user'*) ]]; then
         SUDO='sudo'
     else
-        echo "ERROR: You must either be root or be able to use sudo" >&2
-        #exit 5
+        echo "ERROR: You must either be root or be able to use sudo without a passphrase" >&2
+        exit 5
     fi
 fi
 
